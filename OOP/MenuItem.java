e4e0efbd81c9a1fd8a8edc932a8eb8e53044011a
meta-anjita-maheshwari <anjita.maheshwari@metacube.com>
2015-08-20 19:54:09 +0530

    Create MenuItem.java

---
+++ OOP/MenuItem.java
@@ -0,0 +1,14 @@
+package Assignments;
+
+
+
+/**
+ * @author Anjita
+ * interface for menu function
+ */
+public interface MenuItem {
+	public void display();
+
+	public void takeAction(int choice);
+
+}

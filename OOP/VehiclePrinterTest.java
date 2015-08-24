782195d263eaef51bf9f47415d77ed68cd2bb8ce
meta-anjita-maheshwari <anjita.maheshwari@metacube.com>
2015-08-24 18:55:28 +0530

    Create VehiclePrinterTest.java

---
+++ OOP/VehiclePrinterTest.java
@@ -0,0 +1,20 @@
+package session1;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class VehiclePrinterTest {
+
+	@Test
+	public void test() {
+		// checking for object of car
+		String object2 = "make:Verna,model:verna23";
+		String object1 = VehiclePrinter.printVehicleSpecification(VehicleHelper
+				.createVehicle("Car", "Verna", "verna23"));
+		assertEquals(object1, object2);
+	}
+
+
+}

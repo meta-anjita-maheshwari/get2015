5c4dbebe333e898214789db039cc27aada0fb1ce
meta-anjita-maheshwari <anjita.maheshwari@metacube.com>
2015-08-25 19:08:23 +0530

    Create MainClassLinkedList.java

---
+++ DS/MainClassLinkedList.java
@@ -0,0 +1,36 @@
+package Assignments;
+
+/**
+ * @author Anjita class demonstrating function of linked list
+ */
+public class MainClassLinkedList {
+	public static void main(String args[]) {
+		Node<Integer> a = new Node<Integer>();
+		a.add(5);
+		a.add(96);
+		a.add(1);
+		a.add(87);
+		a.add(-1);
+		System.out.println("\nList elements are :-");
+		a.display();
+
+		System.out.println("\nAdding at position");
+		a.addAtPostion(2, 100);
+		a.display();
+
+		System.out.println("\n\nDeleting item by value");
+		if (a.deleteByValue(96)) {
+
+		} else {
+			System.out.println("no such type of number Exist");
+		}
+		a.display();
+
+		a.deleteByIndex(1);
+		System.out.println("\n\nDeleting item at index 1");
+		a.display();
+
+		System.out.println("\ngetting item");
+		System.out.println(a.getitem(3));
+	}
+}

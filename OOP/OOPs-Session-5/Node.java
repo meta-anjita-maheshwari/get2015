b2be2bed657e4f8a3e10eb04318208a721815707
meta-anjita-maheshwari <anjita.maheshwari@metacube.com>
2015-08-26 18:44:51 +0530

    completed

---
+++ OOP/OOPs-Session-5/Node.java
@@ -0,0 +1,13 @@
+package SocialNetwork;
+
+import java.math.BigInteger;
+
+/**
+ * @author Anjita
+ *
+ */
+public interface Node {//Interface implemented in Entity class
+	public String getName() ;
+	public BigInteger getPhone();
+	public String getEmail();
+}

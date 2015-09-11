808d22388c0d7567345897d1ea0c01c477395f81
meta-anjita-maheshwari <anjita.maheshwari@metacube.com>
2015-09-11 18:37:38 +0530

    Update Assignment2.sql

--- DBMS/Assignment2.sql
+++ DBMS/Assignment2.sql
@@ -3,11 +3,11 @@ use lis;
 
 /*1. select subject name, subject id and number of books purchased*/
 SELECT s.subject_id, s.subject_nm, COUNT(s.subject_id) AS TOTAL_BOOKS
-from books b
-inner join titles t
-inner join subjects s
-where((b.title_id = t.title_id) AND (t.subject_id = s.subject_id))
-group by s.subject_id;
+FROM books b
+INNER JOIN titles t
+INNER JOIN subjects s
+WHERE((b.title_id = t.title_id) AND (t.subject_id = s.subject_id))
+GROUP BY s.subject_id;
 
 /*2. select book_issue information where difference of issue date and due date is more than 2 months*/
 SELECT * FROM book_issue
